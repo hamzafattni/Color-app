445ce42d122741115584e24a6ba74504cb6e27c3
hamzafattni <136331328+hamzafattni@users.noreply.github.com>
2023-09-03 23:08:40 +0500

    Add files via upload

---
+++ Test 11/app.js
@@ -0,0 +1,19 @@
+const red1 = document.querySelector("body")
+function red() {
+    red1.style.backgroundColor = "red"
+}
+
+const red2 = document.querySelector("body")
+function purple() {
+    red2.style.backgroundColor = "purple"
+}
+
+const red3 = document.querySelector("body")
+function blue() {
+    red3.style.backgroundColor = "blue"
+}
+
+const red4 = document.querySelector("body")
+function yellow() {
+    red4.style.backgroundColor = "yellow"
+}
